247df9e629635c0321a277f8bc93d77a7b01b87e
Victoria Hong <xin.victoria.hong@gmail.com>
2016-08-23 18:26:49 -0700

    142

---
+++ easy/n142.java
@@ -0,0 +1,23 @@
+// Linked List Cycle II
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null) return null;
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                ListNode slow2 = head;
+                while (slow2 != slow) {
+                    slow = slow.next;
+                    slow2 = slow2.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+}
+
+// https://discuss.leetcode.com/topic/19367/java-o-1-space-solution-with-detailed-explanation
