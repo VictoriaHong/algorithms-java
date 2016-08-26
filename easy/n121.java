8c294f503c55fd25ee8a0737353441ba17f8a62e
Victoria Hong <xin.victoria.hong@gmail.com>
2016-08-25 23:14:40 -0700

    121

---
+++ easy/n121.java
@@ -0,0 +1,13 @@
+// Best Time to Buy and Sell Stock
+public class Solution {
+    public int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE;
+        int max = 0;
+        
+        for (int i = 0; i < prices.length; i++) {
+            min = Math.min(min, prices[i]);
+            max = Math.max(max, prices[i] - min);
+        }
+        return max;
+    }
+}
