754114515bc8ff6c015a34cc3271100c003010eb
Victoria Hong <xin.victoria.hong@gmail.com>
2016-08-28 20:27:40 -0700

    14

---
+++ easy/n14.java
@@ -0,0 +1,14 @@
+// Longest Common Prefix O(n)
+public class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        String res = strs[0];
+        int n = strs.length;
+        for (int i = 1; i < n; i++) {
+            while (strs[i].indexOf(res) != 0) {
+                res = res.substring(0, res.length() - 1);
+            }
+        }
+        return res;
+    }
+}
