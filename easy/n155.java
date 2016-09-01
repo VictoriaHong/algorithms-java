307b97a4141ea9eb95cd5994fa80691abfa58886
Victoria Hong <xin.victoria.hong@gmail.com>
2016-08-31 20:58:03 -0700

    155

---
+++ easy/n155.java
@@ -0,0 +1,29 @@
+// Min Stack
+public class MinStack {
+    Stack<Integer> stack;
+    Stack<Integer> minStack;
+    /** initialize your data structure here. */
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+    
+    public void push(int x) {
+        stack.push(x);
+        if (minStack.isEmpty()) minStack.push(x);
+        else minStack.push(Math.min(x, minStack.peek()));
+    }
+    
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
