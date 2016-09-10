97fa20dd8e72995e7112030da28f5ab375b30098
Victoria Hong <xin.victoria.hong@gmail.com>
2016-09-09 20:27:46 -0700

    update max subarray

--- medium/Maximum_Subarray_53.java
+++ medium/Maximum_Subarray_53.java
@@ -13,3 +13,22 @@ public class Solution {
         return max;
     }
 }
+
+// 不用前缀和数组
+public class Solution {
+    public int maxSubArray(int[] nums) {
+        int n = nums.length;
+        // 滚动数组
+        // f[] is current max, must include nums[i]
+        // g[] is global max
+        int[] f = new int[2];
+        int[] g = new int[2];
+        f[0] = nums[0];
+        g[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            f[i%2] = Math.max(nums[i], f[(i - 1)%2] + nums[i]);
+            g[i%2] = Math.max(f[i%2], g[(i - 1)%2]);
+        }
+        return g[(n - 1)%2];
+    }
+}
