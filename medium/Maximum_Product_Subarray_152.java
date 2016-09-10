0c35bc4d2c56b793afa9b732bc67ebb3eb666271
Victoria Hong <xin.victoria.hong@gmail.com>
2016-09-09 20:36:49 -0700

    update max product subarray

--- medium/Maximum_Product_Subarray_152.java
+++ medium/Maximum_Product_Subarray_152.java
@@ -27,3 +27,31 @@ public class Solution {
         return Math.min(a, Math.min(b, c));
     }
 }
+
+// 局部最大最小+全局最大
+public class Solution {
+    public int maxProduct(int[] nums) {
+        if (nums == null) return 0;
+        int n = nums.length;
+        int[] curMax = new int[2];
+        int[] curMin = new int[2];
+        int[] g = new int[2];
+        curMax[0] = nums[0];
+        curMin[0] = nums[0];
+        g[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            curMax[i%2] = max(curMax[(i - 1)%2]*nums[i], curMin[(i - 1)%2]*nums[i], nums[i]);
+            curMin[i%2] = min(curMax[(i - 1)%2]*nums[i], curMin[(i - 1)%2]*nums[i], nums[i]);
+            g[i%2] = Math.max(g[(i - 1)%2], curMax[i%2]);
+        }
+        return g[(n - 1)%2];
+    }
+    
+    public int max(int a, int b, int c) {
+        return Math.max(a, Math.max(b, c));
+    }
+    
+    public int min(int a, int b, int c) {
+        return Math.min(a, Math.min(b, c));
+    }
+}
