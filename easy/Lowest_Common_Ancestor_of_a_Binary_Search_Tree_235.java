a12ae616ec5a03df5be96cee037327f96e75e110
Victoria Hong <xin.victoria.hong@gmail.com>
2016-09-12 17:24:49 -0700

    LCA BST

---
+++ easy/Lowest_Common_Ancestor_of_a_Binary_Search_Tree_235.java
@@ -0,0 +1,23 @@
+// Lowest Common Ancestor of a Binary Search Tree
+// recursive
+public class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) return root;
+        
+        if (root.val > p.val && root.val > q.val) return lowestCommonAncestor(root.left, p, q);
+        else if (root.val < p.val && root.val < q.val) return lowestCommonAncestor(root.right, p, q);
+        else return root;
+    }
+}
+
+// iterative
+public class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return root;
+        while (true) {
+            if (root.val > p.val && root.val > q.val) root = root.left;
+            else if (root.val < p.val && root.val < q.val) root = root.right;
+            else return root;
+        }
+    }
+}
